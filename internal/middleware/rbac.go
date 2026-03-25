f3f13568924ef218bad4356ab32970bc0581ff0c
bangipin <ersajogja@gmail.com>
2026-03-25 10:28:03 +0700

    hapus func deprecated

--- internal/middleware/rbac.go
+++ internal/middleware/rbac.go
@@ -7,40 +7,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// func RequirePermission(requiredPermission string) fiber.Handler {
-// 	return func(c *fiber.Ctx) error {
-// 		claims, ok := c.Locals("user").(*domain.AccessClaims)
-// 		if !ok {
-// 			return response.HandleError(c, response.ErrForbiddenAccess)
-// 		}
-
-// 		hasAccess := false
-
-// 		// Superadmin bypass
-// 		for _, r := range claims.Roles {
-// 			if r == "superadmin" {
-// 				hasAccess = true
-// 				break
-// 			}
-// 		}
-
-// 		if !hasAccess {
-// 			for _, p := range claims.Permissions {
-// 				if p == requiredPermission {
-// 					hasAccess = true
-// 					break
-// 				}
-// 			}
-// 		}
-
-// 		if !hasAccess {
-// 			return response.HandleError(c, response.ErrForbiddenAccess)
-// 		}
-
-// 		return c.Next()
-// 	}
-// }
-
 func RequirePermission(permission string, uc domain.PermissionUseCase) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		claims, ok := c.Locals("user").(*domain.AccessClaims)
